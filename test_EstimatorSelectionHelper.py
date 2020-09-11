66ac2745a2c8f027d006cb9ba831b702f2e9565f
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-09-10 20:05:31 -0400

    Removing stashed changes

--- test_EstimatorSelectionHelper.py
+++ test_EstimatorSelectionHelper.py
@@ -3,8 +3,13 @@ from GridSearchHelper import EstimatorSelectionHelper
 
 
 class TestGridSearchHelper(unittest.TestCase):
-<<<<<<< Updated upstream
-    def 
-=======
+    """
+    Things to test and later write assert statements to the main code: 
+    len(models) == len(params)
+
+    for .fit(): X, y should be np.arrays
+    X.shape[0] == y.shape[0]
+    y.shape[1] == 1
+    same for X_train, y_train, X_test, y_test
+    """
     
->>>>>>> Stashed changes
